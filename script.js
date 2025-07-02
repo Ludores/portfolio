cbc28bd3549c30ad44813f7f256307cd6c3b017e
Ludores <bejeryludores@gmail.com>
2025-07-02 09:59:51 +0300

    begin to add  animation sending mail

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ emailjs.init("VLotxSJrdaTeXXa8f");
 
 //////////////////////////////////////////////////////constants//////////////////////////////////////////////////////
 
-
 const portfolio = [
   {
     title: "Mon portfolio ",
@@ -14,7 +13,7 @@ const portfolio = [
     link: "https://www.facebook.com",
     git: "https://www.facebook.com",
     image: "./assets/porfolio.png",
-    imageBorder: true
+    imageBorder: true,
   },
   {
     title: "STR360 ",
@@ -31,7 +30,7 @@ const portfolio = [
     type: "Site Web",
     technologies: ["NextJS", "Scss", "Tailwind", "NestJs"],
     description:
-      "<b>Développement backOffice: </b>Il s’agit d’un système complet de gestion de cashpoints, permettant de suivre en temps réel toutes les transactions financières effectuées dans plusieurs points de service (dépôts, retraits, transferts, paiements, approvisionnements, etc.). Chaque opération est enregistrée avec précision, incluant le montant, l’agent concerné, le solde avant/après, et la nature de la transaction. Grâce à un espace administrateur dédié, le responsable ou \"boss\" peut visualiser à distance l’ensemble des activités de tous les cashpoints, consulter les soldes disponibles, détecter d’éventuels déséquilibres de caisse, générer des rapports financiers et assurer un contrôle global sur les opérations quotidiennes, assurant ainsi une traçabilité, une sécurité et une vision centralisée de toute l’activité du réseau.",
+      '<b>Développement backOffice: </b>Il s’agit d’un système complet de gestion de cashpoints, permettant de suivre en temps réel toutes les transactions financières effectuées dans plusieurs points de service (dépôts, retraits, transferts, paiements, approvisionnements, etc.). Chaque opération est enregistrée avec précision, incluant le montant, l’agent concerné, le solde avant/après, et la nature de la transaction. Grâce à un espace administrateur dédié, le responsable ou "boss" peut visualiser à distance l’ensemble des activités de tous les cashpoints, consulter les soldes disponibles, détecter d’éventuels déséquilibres de caisse, générer des rapports financiers et assurer un contrôle global sur les opérations quotidiennes, assurant ainsi une traçabilité, une sécurité et une vision centralisée de toute l’activité du réseau.',
     link: "https://www.facebook.com",
     git: "https://www.facebook.com",
     image: "./assets/back-office.png",
@@ -55,23 +54,23 @@ const portfolio = [
     link: "https://www.facebook.com",
     git: "https://www.facebook.com",
     image: "./assets/fs-manager.jpeg",
-  }
+  },
 ];
 const technologies = [
-  {url: "./assets/react.svg", name: "ReactJs"},
-  {url: "./assets/vue.svg", name: "VueJs"},
-  {url: "./assets/next.svg", name: "NextJs"},
-  {url: "./assets/nuxt.svg", name: "NuxtJs"},
-  {url: "./assets/react.svg", name: "Native"},
-  {url: "./assets/express.svg", name: "ExpressJs"},
-  {url: "./assets/nest.svg", name: "NestJs"},
-  {url: "./assets/mongo.svg", name: "MongoDB"},
-  {url: "./assets/mysql.svg", name: "MySQL"},
-  {url: "./assets/db.svg", name: "PostgreSQl"},
-  {url: "./assets/git.svg", name: "Git"},
-  {url: "./assets/electron.svg", name: "Electron"},
-  {url: "./assets/indexeddb.svg", name: "IndexedDB"},
-]
+  { url: "./assets/react.svg", name: "ReactJs" },
+  { url: "./assets/vue.svg", name: "VueJs" },
+  { url: "./assets/next.svg", name: "NextJs" },
+  { url: "./assets/nuxt.svg", name: "NuxtJs" },
+  { url: "./assets/react.svg", name: "Native" },
+  { url: "./assets/express.svg", name: "ExpressJs" },
+  { url: "./assets/nest.svg", name: "NestJs" },
+  { url: "./assets/mongo.svg", name: "MongoDB" },
+  { url: "./assets/mysql.svg", name: "MySQL" },
+  { url: "./assets/db.svg", name: "PostgreSQl" },
+  { url: "./assets/git.svg", name: "Git" },
+  { url: "./assets/electron.svg", name: "Electron" },
+  { url: "./assets/indexeddb.svg", name: "IndexedDB" },
+];
 
 const show_nav_ul = () => {
   const nav_ul = document.querySelector("nav>ul");
@@ -92,10 +91,9 @@ const showMoreDescription = () => {
   const descriptionSecondary = document.querySelectorAll(
     ".section-2>.description>.description-secondary"
   );
-  const voirPlus = document.querySelector(".plus")
+  const voirPlus = document.querySelector(".plus");
 
   descriptionSecondary.forEach((element, i) => {
-
     element.classList.toggle("show");
   });
 };
@@ -104,10 +102,9 @@ const DomContentLoaded = () => {
   loading.style.display = "none";
 
   setPortfolio();
-  setTechnologies()
-  AOS.refreshHard()
-
-}
+  setTechnologies();
+  AOS.refreshHard();
+};
 
 const setPortfolio = () => {
   const section7 = document.querySelector(".section-7");
@@ -125,33 +122,33 @@ const setPortfolio = () => {
     const div1132 = document.createElement("div");
     const div11322 = document.createElement("div");
     const div12 = document.createElement("div");
-    const img = document.createElement("img")
-    img.src = _portfolio.image
+    const img = document.createElement("img");
+    img.src = _portfolio.image;
 
     div11111.innerHTML = _portfolio.title;
     div11112.innerHTML = _portfolio.type;
     div112.innerHTML = _portfolio.description;
     div1131.innerHTML = "Voir le projet";
     div11322.innerHTML = "gitHub";
-    div11.setAttribute("data-aos", "fade-right")
-    div12.setAttribute("data-aos", "fade-left")
+    div11.setAttribute("data-aos", "fade-right");
+    div12.setAttribute("data-aos", "fade-left");
 
-    if(_portfolio.imageBorder)div12.style.border = "1px solid black"
+    if (_portfolio.imageBorder) div12.style.border = "1px solid black";
     for (const techno of _portfolio.technologies) {
       const div = document.createElement("div");
       div.innerHTML = techno;
       div1112.appendChild(div);
     }
-    
+
     // div1132.appendChild(svgGit)
-    div1111.appendChild(div11111)
-    div1111.appendChild(div11112)
-    div1132.appendChild(div11322)
+    div1111.appendChild(div11111);
+    div1111.appendChild(div11112);
+    div1132.appendChild(div11322);
     div113.appendChild(div1131);
     div113.appendChild(div1132);
     div111.appendChild(div1111);
     div111.appendChild(div1112);
-    div12.append(img)
+    div12.append(img);
     div11.appendChild(div111);
     div11.appendChild(div112);
     div11.appendChild(div113);
@@ -162,57 +159,62 @@ const setPortfolio = () => {
 };
 
 const setTechnologies = () => {
+  const technoContainer = document.querySelector(".section-8>div:last-child");
+  for (const techno of technologies) {
+    const divTechnoItem = document.createElement("div");
 
-  const technoContainer = document.querySelector(".section-8>div:last-child")
-  for(const techno of technologies){
-    const divTechnoItem = document.createElement("div")
+    const image = document.createElement("div");
+    image.style.backgroundImage = `url(${techno.url})`;
+    const divName = document.createElement("div");
+    divName.innerHTML = techno.name;
 
-    const image = document.createElement("div")
-    image.style.backgroundImage = `url(${techno.url})`  
-    const divName = document.createElement("div")
-    divName.innerHTML = techno.name
+    divTechnoItem.appendChild(image);
+    divTechnoItem.appendChild(divName);
+    divTechnoItem.style.zIndex = 3;
 
-    divTechnoItem.appendChild(image)
-    divTechnoItem.appendChild(divName)
-    divTechnoItem.style.zIndex = 3
-
-    technoContainer.appendChild(divTechnoItem)
+    technoContainer.appendChild(divTechnoItem);
   }
-}
+};
 
 const enregistrer = () => {
-  console.log("xxx")
+  console.log("xxx");
 
-  const params = {
-    from_name: "Ludores",
-    message: "Ceci est un message sans formulaire.",
-    reply_to: "bejeryludores@gmail.com"
-  };
-  // emailjs.send("service_fo202e9", "template_tsitqmu", params, "VLotxSJrdaTeXXa8f")
-  // .then(function(response) {
-  //   alert("E-mail envoyé avec succès !");
-  //   console.log("SUCCESS!", response.status, response.text);
-  // }, function(error) {
-  //   alert("Erreur lors de l'envoi.");
-  //   console.log("FAILED...", error);
-  // });
-  emailjs.send("service_e00ayg6", "template_tsitqmu", {
-        from_name: "bejery",
-        reply_to: "larrysrakoto@gmail.com",
-        message: "nous avons besoin de vous pour une sevice qu'on veut mettre en place"
-      }).then(
-        function(response) {
-          alert("Message envoyé !");
-        },
-        function(error) {
-          alert("Erreur : " + error.text); // Ici s'affichera "412" si tu as mal écrit un champ
-        }
-      );
-}
+  const loading = document.querySelector(".loading");
+  const svg1 = document.querySelector(".loading>div:first-child>div:first-child>svg:first-child");
+  const svg2 = document.querySelector(".loading>div:first-child>div:first-child>svg:last-child");
+  const text = document.querySelector(".loading>div:first-child>div:last-child")
+  const divRotate = document.querySelector(".loading>div:first-child>div:first-child")
+
+  loading.style.display = "flex";
+  svg1.style.display = "block";
+  svg2.style.display = "none";
+  text.innerHTML = "Envoie du mail"
+
+  emailjs
+    .send("service_e00ayg6", "template_tsitqmu", {
+      from_name: "bejery",
+      reply_to: "larrysrakoto@gmail.com",
+      message:
+        "nous avons besoin de vous pour une sevice qu'on veut mettre en place",
+    })
+    .then(
+      function (response) {
+        svg1.style.display = "none";
+        svg2.style.display = "block";
+        text.innerHTML = "Mail envoyé !"
+        divRotate.style.animation = ""
+        setTimeout(() => {
+          divRotate.style.animation = "load 3s infinite linear"
+          loading.style.display = "none";
+        }, 1000);
+      },
+      function (error) {
+        alert("Erreur : " + error.text); // Ici s'affichera "412" si tu as mal écrit un champ
+      }
+    );
+};
 
 //event Listener
 document.addEventListener("scroll", scrollEvent);
 
 document.addEventListener("DOMContentLoaded", DomContentLoaded);
-
-
